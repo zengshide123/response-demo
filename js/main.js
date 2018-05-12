956fe1c6b9a8cb2abd22f7cafb850c24d0fdf0ec
liu lang yi ge <1909493019@qq.com>
2018-05-12 21:37:47 +0800

    初次提交交

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(()=>{
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveRefreshRate:100
+    })
+})
